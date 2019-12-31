98e0fa58343c82743e7a4962e6ccf612c3ed91e8
yugoueda <s16c1019gl@s.chibakoudai.jp>
2019-12-31 19:01:45 +0900

    new file

--- myled.c
+++ myled.c
@@ -14,13 +14,86 @@ MODULE_DESCRIPTION("driver for LED control");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
 
-static int __init init_mod(void) //カーネルモジュールの初期化
+static dev_t dev;
+static struct cdev cdv;
+static struct class *cls = NULL;
+static volatile u32 *gpio_base = NULL;
+
+static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
-        return 0;
+  char c;
+  int i = 0;
+  
+  if(copy_from_user(&c,buf,sizeof(char)))
+  return -EFAULT;
+  if(c && c != '\n'){
+    if( 0 <= (int) c -(int) '0' && (int) c - (int) '0' <= 9){
+        for(i=0; i<((int) c - (int) '0'); i++){ 
+        gpio_base[7] = 1 << 25;
+        msleep(((int) c - (int) '0')*1000);
+        gpio_base[10] = 1 << 25;
+        msleep(500);
+        }
+    }
+    else{ 
+      printk(KERN_INFO "no number!\n");
+      gpio_base[7] = 1 << 25;
+      msleep(1000);
+      }
+    }
+  }
+
+  printk(KERN_INFO "recive:%c\n",c);
+  gpio_base[10] = 1 << 25;
+  return 1;
 }
 
-static void __exit cleanup_mod(void) //後始末
+
+
+static struct file_operations led_fops = {
+  .owner = THIS_MODULE,
+  .write = led_write,
+};
+
+static int __init init_mod(void)
 {
+  int retval;
+
+  const u32 led = 25;
+  const u32 index = led/10;
+  const u32 shift = (led%10)*3;
+  const u32 mask = ~(0x7 << shift);
+  gpio_base = ioremap_nocache(0x3f200000,0xA0);
+  gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+
+  retval = alloc_chrdev_region(&dev,0,1,"myled");
+  if (retval<0){
+    printk(KERN_ERR "alloc region failed.\n");
+    return retval;
+  }
+  printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
+  cdev_init(&cdv, &led_fops);
+  retval = cdev_add(&cdv, dev, 1);
+  if(retval < 0){
+    printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
+    return retval;
+  }
+  cls = class_create(THIS_MODULE,"myled");   
+  if(IS_ERR(cls)){
+    printk(KERN_ERR "class_create failed.");
+    return PTR_ERR(cls);
+  }
+  device_create(cls,NULL,dev,NULL,"myled%d",MINOR(dev));
+  return 0;
+}
+
+static void __exit cleanup_mod(void) 
+{
+  cdev_del(&cdv);
+  device_destroy(cls,dev);
+  class_destroy(cls);
+  unregister_chrdev_region(dev, 1);
+  printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
 }
 
 module_init(init_mod);     
